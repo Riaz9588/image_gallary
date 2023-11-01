5012a3c9a874ad57fd946007395c29d2b0e23391
Rohoul Kuddus Riaz <mdriaz9586@gmail.com>
2023-11-02 01:34:44 +0600

    new update

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
-import { BsPlusCircle, BsTrash } from 'react-icons/bs';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { RiImageAddLine } from 'react-icons/ri';
+import { BsTrash } from 'react-icons/bs';
 
-const Gallery = () => {
+const ImageGallery = () => {
   const [images, setImages] = useState([]);
   const [selectedImages, setSelectedImages] = useState(new Set());
 
-  const handleImageUpload = (e) => {
-    const files = e.target.files;
-    if (files) {
-      const updatedImages = [...images];
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        const reader = new FileReader();
-        reader.onload = (event) => {
-          updatedImages.push(event.target.result);
-          setImages(updatedImages);
-        };
-        reader.readAsDataURL(file);
-      }
-    }
+  // drag and drop functionality - starts
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData("index", index);
   };
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+  const handleDrop = (e, index) => {
+    const droppedIndex = e.dataTransfer.getData("index");
+    const newImages = [...images];
+    const draggedImage = newImages[droppedIndex];
+    newImages.splice(droppedIndex, 1);
+    newImages.splice(index, 0, draggedImage);
+    setImages(newImages);
+  };
+  // drag and drop functionality - ends
 
+  // checkbox functionality - starts
   const handleCheckboxChange = (index) => {
     const updatedSelectedImages = new Set(selectedImages);
     if (updatedSelectedImages.has(index)) {
@@ -31,27 +33,29 @@ const Gallery = () => {
     }
     setSelectedImages(updatedSelectedImages);
   };
-
   const handleDeleteSelected = () => {
     const updatedImages = images.filter((_, index) => !selectedImages.has(index));
     setImages(updatedImages);
     setSelectedImages(new Set());
   };
+  // checkbox functionality - ends
 
-  const onDragEnd = (result, provided) => {
-    if (!result.destination) return;
-
-    const items = Array.from(images);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setImages(items);
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        setImages([...images, event.target.result]);
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
   return (
     <div className="">
+      {/* navbar starts */}
       {selectedImages.size > 0 && (
-        <div className="shadow-lg bg-white">
+        <div className="shadow-sm bg-white">
           <div className="sticky top-0 z-10 container mx-auto text-black font-semibold p-4 flex justify-between items-center">
             <div>Selected Images: {selectedImages.size}</div>
             {selectedImages.size > 0 && (
@@ -62,56 +66,29 @@ const Gallery = () => {
           </div>
         </div>
       )}
-      <div className="container mx-auto p-4">
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="galleryID" direction="horizontal">
-            {(provided) => (
-              <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                className="galleryID grid grid-cols-5 gap-4 mt-4 relative container mx-auto"
-              >
-                {images.map((image, index) => (
-                  <Draggable key={index} draggableId={`image-${index}`} index={index}>
-                    {(provided) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        className={`relative shadow-lg ${index === 0 ? 'col-span-2 row-span-2' : ''}`}
-                      >
-                        <input
-                          type="checkbox"
-                          checked={selectedImages.has(index)}
-                          onChange={() => handleCheckboxChange(index)}
-                          className="absolute top-2 left-2"
-                        />
-                        <img src={image} alt={`Image ${index}`} className="w-full h-full" />
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-                <div className="flex items-center justify-center bg-gray-200 rounded-md h-72">
-                  <label htmlFor="fileInput" className="cursor-pointer">
-                    <BsPlusCircle className="h-20 w-20 text-gray-400" />
-                  </label>
-                  <input
-                    id="fileInput"
-                    type="file"
-                    className="hidden"
-                    accept="image/*"
-                    multiple
-                    onChange={handleImageUpload}
-                  />
-                </div>
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
+      {/* navbar ends */}
+
+      <div className="grid sm:grid-cols-4 md:grid-cols-6 gap-4 container mx-auto p-4 mt-4" onDragOver={(e) => handleDragOver(e)}>
+        {images.map((image, index) => (
+          <div
+            key={index}
+            draggable
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDrop={(e) => handleDrop(e, index)}
+            className={`relative cursor-pointer shadow-lg ${index === 0 && 'col-span-2 row-span-2'}`}
+          >
+            <input type="checkbox" checked={selectedImages.has(index)} onChange={() => handleCheckboxChange(index)} className="absolute top-2 left-2" />
+            <img src={image} alt={`Image ${index}`} className={`w-full object-cover ${index === 0 ? 'h-[336px]' : 'h-40'}`} />
+          </div>
+        ))}
+        <div className="flex items-center justify-center bg-white shadow-lg rounded-md h-40">
+          <label htmlFor="fileInput" className="cursor-pointer font-semibold text-gray-400"><RiImageAddLine className="h-20 w-20" />Add Image</label>
+          <input id="fileInput" type="file" className="hidden" accept="image/*" multiple onChange={handleImageUpload} />
+        </div>
       </div>
+
     </div>
   );
 };
 
-export default Gallery;
+export default ImageGallery;
