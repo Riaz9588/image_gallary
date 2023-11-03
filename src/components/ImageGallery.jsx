06839470e731ce64bfa74c6debed21720b742e4e
Riaz9588 <mdriaz9588@gmail.com>
2023-11-03 12:24:07 +0600

    update the boxes

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -55,14 +55,20 @@ const ImageGallery = () => {
   return (
     <>
       {/* navbar starts */}
-      {selectedImages.size > 0 && (
-        <div className="shadow-sm bg-white">
-          <div className="container mx-auto text-black font-semibold p-4 flex justify-between items-center">
+
+      <div className="shadow-sm bg-white">
+        {selectedImages.size > 0 ?
+          <div className="container mx-auto font-semibold p-4 flex justify-between items-center">
             <div className="shadow p-2 rounded-md bg-white text-blue-500 font-bold flex items-center uppercase"><BiSolidSelectMultiple className='mt-1 mr-1' />Selected Images:<span className="font-black ml-1">{selectedImages.size}</span></div>
             <button onClick={handleDeleteSelected} className="p-2 rounded-full bg-red-500 hover:bg-red-600 text-white"><BsTrash className="h-6 w-6" /></button>
           </div>
-        </div>
-      )}
+          :
+          <div className="container mx-auto text-blue-500 font-semibold p-4">
+            <span className="font-black ml-1 text-3xl">Image Gallery</span>
+          </div>
+        }
+      </div>
+
       {/* navbar ends */}
 
       <div className="grid sm:grid-cols-4 md:grid-cols-6 gap-4 container mx-auto p-4 mt-4" onDragOver={(e) => handleDragOver(e)}>
@@ -72,14 +78,14 @@ const ImageGallery = () => {
             draggable
             onDragStart={(e) => handleDragStart(e, index)}
             onDrop={(e) => handleDrop(e, index)}
-            className={`group relative cursor-pointer shadow-lg ${index === 0 && 'col-span-2 row-span-2'}`}
+            className={`group relative cursor-pointer shadow-lg rounded-md transition hover:scale-105 ${index === 0 && 'col-span-2 row-span-2'}`}
           >
             <input type="checkbox" checked={selectedImages.has(index)} onChange={() => handleCheckboxChange(index)} className="absolute top-2 left-2 z-10" />
-            <img src={image} alt={`Image ${index}`} className={`w-full object-cover ${index === 0 ? 'h-[336px]' : 'h-40'}`} />
-            <div className="absolute inset-0 bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 transition-opacity duration-700"></div>
+            <img src={image} alt={`Image ${index}`} className={`w-full object-contain rounded-md ${index === 0 ? 'aspect-w-2 aspect-h-2' : 'aspect-w-1 aspect-h-1'}`} />
+            <div className="absolute inset-0 bg-black rounded-md bg-opacity-30 opacity-0 group-hover:opacity-100 transition-opacity duration-700"></div>
           </div>
         ))}
-        <div className="flex items-center justify-center bg-white shadow-lg rounded-md h-40">
+        <div className="flex items-center justify-center bg-white shadow-lg rounded-md cursor-pointer transition hover:scale-105" style={{ aspectRatio: '1/1' }}>
           <label htmlFor="fileInput" className="cursor-pointer font-semibold text-gray-400"><RiImageAddLine className="h-20 w-20" />Add Images</label>
           <input id="fileInput" type="file" className="hidden" accept="image/*" multiple onChange={handleImageUpload} />
         </div>
